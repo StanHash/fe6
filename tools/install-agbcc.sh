a8c452013dcc3d65940eff24ba4d28dcac5a1bad
laqieer <laqieer@126.com>
2023-03-23 13:47:17 +0800

    support project build on Mac, resolves https://github.com/StanHash/fe6/issues/8

--- tools/install-agbcc.sh
+++ tools/install-agbcc.sh
@@ -3,10 +3,19 @@
 here=$(dirname "$(readlink -f "$0")")/..
 temp=$(mktemp -d)
 
-git clone https://github.com/StanHash/agbcc.git $temp
+if [[ "$OSTYPE" == "darwin"* ]]
+then
+    agbcc_repo="https://github.com/holmesmr/agbcc.git"
+    agbcc_branch="master"
+else
+    agbcc_repo="https://github.com/StanHash/agbcc.git"
+    agbcc_branch="fix-dwarf-debug-line"
+fi
+
+git clone $agbcc_repo $temp
 
 cd $temp
-git checkout origin/fix-dwarf-debug-line
+git checkout origin/$agbcc_branch
 ./build.sh
 ./install.sh $here
 
